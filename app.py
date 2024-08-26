10a6d019e988d5d253896108589c619ca2c3967e
Grampers <136607222+Grampers-Dev@users.noreply.github.com>
2024-08-26 17:18:32 +0100

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from app_pages.page_emission_classification import page_emission_classification_
 from app_pages.page_project_hypothesis import page_project_hypothesis_body
 from app_pages.page_predict_co2 import page_predict_co2_body
 from app_pages.page_cluster import page_cluster_body
-from app_pages.page_co2_predictor import page_co2_predictor_body
+#from app_pages.page_co2_predictor import page_co2_predictor_body
 
 # Create an instance of the app
 app = MultiPage(app_name="CO2Oracle: CO2 Emissions Analysis")
@@ -20,7 +20,7 @@ app.add_page("CO2 Emission Classification", page_emission_classification_body)
 app.add_page("Project Hypothesis and Validation", page_project_hypothesis_body)
 app.add_page("ML: Predict Co2", page_predict_co2_body)
 app.add_page("CO2 Cluster Analysis", page_cluster_body)
-app.add_page("CO2 Emissions Predictor", page_co2_predictor_body)
+#app.add_page("CO2 Emissions Predictor", page_co2_predictor_body)
 
 
 # Run the app
