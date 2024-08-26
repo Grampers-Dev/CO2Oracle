63d45c294e0b9c36643b123d6e75ca5b0f981d65
Grampers <136607222+Grampers-Dev@users.noreply.github.com>
2024-08-26 16:19:28 +0100

    Update app.py to exclude page_predict. Possibly will remove from project.

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from app_pages.page_summary import page_summary_body
 from app_pages.page_emission_analysis import page_emission_analysis_body
 from app_pages.page_emission_classification import page_emission_classification_body
 from app_pages.page_project_hypothesis import page_project_hypothesis_body
-from app_pages.page_predict_co2 import page_predict_co2_body
+#from app_pages.page_predict_co2 import page_predict_co2_body
 from app_pages.page_cluster import page_cluster_body
 from app_pages.page_co2_predictor import page_co2_predictor_body
 
@@ -18,7 +18,7 @@ app.add_page("Quick Project Summary", page_summary_body)
 app.add_page("CO2 Emission Analysis", page_emission_analysis_body)
 app.add_page("CO2 Emission Classification", page_emission_classification_body)
 app.add_page("Project Hypothesis and Validation", page_project_hypothesis_body)
-app.add_page("ML: Predict Co2", page_predict_co2_body)
+#app.add_page("ML: Predict Co2", page_predict_co2_body)
 app.add_page("CO2 Cluster Analysis", page_cluster_body)
 app.add_page("CO2 Emissions Predictor", page_co2_predictor_body)
 
